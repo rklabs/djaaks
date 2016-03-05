185ab40e225b2846cd165bc6cc3850b615b5264b
Raju Kadam <rajulkadam@gmail.com>
2016-03-04 23:42:18 -0800

    Add simple test

--- djview/tests.py
+++ djview/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from djview.models import Category
 
-# Create your tests here.
+
+class CategoryMethodTests(TestCase):
+
+    def test_ensure_views_are_positive(self):
+        cat = Category(name='test', views=-1, likes=0)
+        cat.save()
+
+        self.assertEqual((cat.views < 0), True)
